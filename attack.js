59744ff04ae58d6dfd108ed5adbc97c60cf9d648
Manav Patel <74655389+Mpatel1803@users.noreply.github.com>
2023-04-14 11:26:08 +0300

    revert lint fix to break action

--- attack.js
+++ attack.js
@@ -1,3 +1,6 @@
-const attack = (name, damage, isCritical) => `${name} dealt ${isCritical ? damage * 2 : damage} damage!`;
+const attack = (name, damage, isCritical) => {
+	return `${name} dealt ${isCritical ? damage * 2 : damage} damage!`
+}
 
-module.exports = attack;
+module.exports = attack
+
