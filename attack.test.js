908832abc10908252793bc24f9ead7f1b44f3cc8
Manav Patel <74655389+Mpatel1803@users.noreply.github.com>
2023-04-14 12:09:51 +0300

    add jest and ci for it

---
+++ attack.test.js
@@ -0,0 +1,9 @@
+const attack = require('./attack');
+
+test('should return damage sentence correctly when called correctly', () => {
+  const result = attack('Pikachu', 23);
+  expect(result).toBe('Pikachu dealt 23 damage!');
+  expect(result).toMatch(/^Pikachu(.*)23/);
+  expect(result).toMatchInlineSnapshot(`"Pikachu dealt 23 damage!"`);
+
+});
