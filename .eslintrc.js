b9a4768356990afecd36dd59a605a57783ca45a2
Manav Patel <74655389+Mpatel1803@users.noreply.github.com>
2023-04-15 09:01:44 +0300

    Add webpack and babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,11 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
+  extends: [
+    'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
   },
   rules: {
   },
