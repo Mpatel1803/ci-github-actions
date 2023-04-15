331199ce8f41d27ba4f5df14687ca21b56b7e12b
Manav Patel <74655389+Mpatel1803@users.noreply.github.com>
2023-04-15 09:34:16 +0300

    Add html functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import attack from './attack';
 
-console.log(attack('Pikachu', 23));
+// console.log(attack('Pikachu', 23));
+
+const resultTag = document.querySelector('#resultTag');
+const creature = document.querySelector('#creature');
+const damage = document.querySelector('#damage');
+const resultButton = document.querySelector('#resultButton');
+
+resultButton.addEventListener('click', () => {
+  const result = attack(creature.value, damage.value);
+  resultTag.innerHTML = result;
+});
