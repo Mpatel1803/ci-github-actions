e0d43ebff501dc8c9c2c75c4bc5f92ef1d384633
Manav Patel <74655389+Mpatel1803@users.noreply.github.com>
2023-04-15 10:38:09 +0300

    Test to see if deployment fails on test failure

--- src/attack.test.js
+++ src/attack.test.js
@@ -1,7 +1,7 @@
 import attack from './attack';
 
 test('should return damage sentence correctly when called correctly', () => {
-  const result = attack('Pikachu', 23);
+  const result = attack('Pikachu', 22);
   expect(result).toBe('Pikachu dealt 23 damage!');
   expect(result).toMatch(/^Pikachu(.*)23/);
   expect(result).toMatchInlineSnapshot('"Pikachu dealt 23 damage!"');
